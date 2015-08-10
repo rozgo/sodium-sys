9c74f9968514f64ecdae065e7059fa7615a6543f
Jason Ozias <jason.g.ozias@gmail.com>
2015-08-10 16:36:35 -0400

    aead wip: Need to complete benches

--- src/crypto/aead/crypto_aead_chacha20poly1305.rs
+++ src/crypto/aead/crypto_aead_chacha20poly1305.rs
@@ -6,5 +6,8 @@ pub const KEYBYTES: usize = 32;
 // pub const NSECBYTES: usize = 0;
 /// 8 bytes.
 pub const NPUBBYTES: usize = 8;
+#[cfg(feature = "latest")]
+/// 12 bytes.
+pub const IETF_NPUBBYTES: usize = 12;
 /// 16 bytes.
 pub const ABYTES: usize = 16;
