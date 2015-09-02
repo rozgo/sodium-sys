a0bdae058809e4118146b590099a5831af42c1ff
Jason Ozias <jason.g.ozias@gmail.com>
2015-09-02 19:43:08 -0400

    added keypair secretkey bench

--- benches/crypto/crypto_keypair.rs
+++ benches/crypto/crypto_keypair.rs
@@ -1,7 +1,9 @@
+use sodium_sys::utils;
 use sodium_sys::crypto::{keypair,box_};
 use test::Bencher;
 
 const TEST_SEED: [u8; box_::SEEDBYTES] = [0; box_::SEEDBYTES];
+const TEST_OTHER_PK: [u8; box_::PUBLICKEYBYTES] = [0; box_::PUBLICKEYBYTES];
 
 #[bench]
 fn bench_keypair(b: &mut Bencher) {
@@ -22,6 +24,7 @@ fn bench_keypair_seed(b: &mut Bencher) {
 #[bench]
 fn bench_keypair_derive(b: &mut Bencher) {
     ::test_init();
+
     // Setup an initial keypair.
     let ikp = keypair::KeyPair::new(box_::SECRETKEYBYTES,box_::PUBLICKEYBYTES).unwrap();
 
@@ -33,5 +36,22 @@ fn bench_keypair_derive(b: &mut Bencher) {
         keypair::KeyPair::derivepk(&mut ikp.sk_bytes_mut(),
                                    box_::SECRETKEYBYTES,
                                    box_::PUBLICKEYBYTES).unwrap()
+    });
+}
+
+#[bench]
+fn bench_keypair_sharedsecret(b: &mut Bencher) {
+    ::test_init();
+
+    // Setup an initial keypair.
+    let ikp = keypair::KeyPair::new(box_::SECRETKEYBYTES,box_::PUBLICKEYBYTES).unwrap();
+
+    // Activate the keys.
+    ikp.activate_pk();
+    ikp.activate_sk();
+
+    b.iter(|| {
+        let mut ssk = ikp.shared_secret(&TEST_OTHER_PK).unwrap();
+        utils::free(&mut ssk);
     });
 }
